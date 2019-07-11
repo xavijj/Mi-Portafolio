886270bcf39171bf41034faccdb963ffbbd434f2
xavijj <xavi.jj19@gmail.com>
2019-07-11 15:25:06 -0600

    Nuevo

--- js/script.js
+++ js/script.js
@@ -43,3 +43,26 @@ function miFuncion(){
  }
 
 }
+
+$(document).ready(function() {
+  console.log("document ready");
+  $(document).on('click', '.boton-movil', function() {
+    if ($("li").hasClass("menu-listitem")) {
+      console.log("true");
+      $(".menu-listitem").slideDown("slow").addClass("menu-listMovil").removeClass("menu-listitem");
+    } else {
+      console.log("false");
+      $(".menu-listMovil").slideUp("slow").addClass("menu-listitem");
+      //$(".menu-listitem").removeClass("menu-listMovil");
+    };
+
+    console.log("click");
+    $(window).on("resize", function() {
+      if ($(window).width() > 500) {
+        console.log("pantalla chica");
+        $("li").css('display', '').removeClass("menu-listMovil");
+      }
+    }).resize();
+  });
+
+});
